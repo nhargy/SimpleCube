134f3fd650bf871e8866823b75413a18d2fc3867
oviraptor <DIGI.I4ZJZD87@PM.ME>
2025-12-18 15:04:45 +0200

    Fixed seed bug

--- job_submission/run_SimpleCube_single.sh
+++ job_submission/run_SimpleCube_single.sh
@@ -15,7 +15,7 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc12-opt/setup.sh
 echo " "
 echo "=====> Running GEANT4 SimpleCube Sim! <====="    
 cd /srv01/xenon/{{USER}}/SimpleCube/build
-./SimpleCube --macro {{MACROFILE}} --primaries {{PRIMARIES}} --outfile /storage/xenon/{{USER}}/SimpleCube/{{DIRNAME}}/raw/{{OUTFILENAME}}
+./SimpleCube --macro {{MACROFILE}} --primaries {{PRIMARIES}} --outfile /storage/xenon/{{USER}}/SimpleCube/{{DIRNAME}}/raw/{{OUTFILENAME}} --seed {{SEED}}
 
 echo "=====> Processing to .parquet format"    
 python ../analysis/post_processing.py --rootfile /storage/xenon/{{USER}}/SimpleCube/{{DIRNAME}}/raw/{{OUTFILENAME}} --outfile /storage/xenon/{{USER}}/SimpleCube/{{DIRNAME}}/proc/{{PROCFILENAME}}
